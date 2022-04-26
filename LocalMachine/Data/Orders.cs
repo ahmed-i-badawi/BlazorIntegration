48bf88fcf608912541cdd8d8463a25701e9f115b
Ahmed Badawi <ahmedbadawi127@gmail.com>
2022-04-26 02:15:50 +0200

    refactor

--- LocalMachine/Data/Orders.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace LocalMachine.Data
-{
-    public class Orders
-    {
-        public int Id { get; set; }
-        public string ConnectionId { get; set; }
-        public string ProductInfo { get; set; }
-    }
-}
